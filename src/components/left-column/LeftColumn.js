5d3310225d288a2615d42cd575d35b16fec6a270
Dhiogo Boza <dhiogoboza@gmail.com>
2022-01-22 16:17:20 -0300

    Removing skills section until it is complete

--- src/components/left-column/LeftColumn.js
+++ src/components/left-column/LeftColumn.js
@@ -37,27 +37,6 @@ class LeftColumn extends React.Component {
           <p><i className="fa fa-phone fa-fw w3-margin-right w3-large"></i>{ this.userInfo.phoneNumber }</p>
           <hr />
 
-          <p className="w3-large"><b><i className="fa fa-asterisk fa-fw w3-margin-right"></i>Skills</b></p>
-          <p>Adobe Photoshop</p>
-          <div className="w3-light-grey w3-round-xlarge w3-small">
-            <div className="w3-container w3-center w3-round-xlarge w3-teal" style={{width:"90%"}}>90%</div>
-          </div>
-          <p>Photography</p>
-          <div className="w3-light-grey w3-round-xlarge w3-small">
-            <div className="w3-container w3-center w3-round-xlarge w3-teal" style={{width:"80%"}}>
-              <div className="w3-center w3-text-white">80%</div>
-            </div>
-          </div>
-          <p>Illustrator</p>
-          <div className="w3-light-grey w3-round-xlarge w3-small">
-            <div className="w3-container w3-center w3-round-xlarge w3-teal" style={{width:"75%"}}>75%</div>
-          </div>
-          <p>Media</p>
-          <div className="w3-light-grey w3-round-xlarge w3-small">
-            <div className="w3-container w3-center w3-round-xlarge w3-teal" style={{width:"50%"}}>50%</div>
-          </div>
-          <br />
-
           <p className="w3-large"><b><i className="fa fa-globe fa-fw w3-margin-right"></i>Languages</b></p>
           { this.languages() }
           <br />
